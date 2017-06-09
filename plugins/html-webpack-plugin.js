310e2ce978fa0446611c14d54c62460792a32a84
LiGuoming <it1011@qq.com>
2017-06-09 14:47:26 +0800

    修复html文件引用不对的问题

--- plugins/html-webpack-plugin.js
+++ plugins/html-webpack-plugin.js
@@ -2,7 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = new HtmlWebpackPlugin({
-    template: path.resolve(__dirname, "index.html"),
+    template: path.resolve(__dirname, "../index.html"),
     filename: "index.html",
     inject: "body"
 });
