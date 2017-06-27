84bcd7750e4742c3f67f77088375b61501fc1b27
LiGuoming <it1011@qq.com>
2017-06-27 18:52:09 +0800

    添加正则排除@beisen/talent-ui目录

--- rules/js-rule.js
+++ rules/js-rule.js
@@ -1,12 +1,18 @@
-const { targetBrowsers, targets:tgt, transformInclude, transformExclude, buildProd } = global["talent-ui-runtime"];
+const {
+    targetBrowsers,
+    targets: tgt,
+    transformInclude,
+    transformExclude,
+    buildProd
+} = global["talent-ui-runtime"];
 
 targets = tgt || {
-        browsers: targetBrowsers
-}
+    browsers: targetBrowsers || 'chrome >= 55'
+};
 
 module.exports = {
     test: /\.(js)$/,
-    exclude: /node_modules/,
+    exclude: /node_modules\/(?!@beisen\/talent-ui)/, //本正则由张跃同学提供
     loader: "babel-loader",
     options: {
         babelrc: false,
@@ -20,7 +26,7 @@ module.exports = {
                     include: transformInclude,
                     exclude: transformExclude,
                     useBuiltIns: true,
-                    debug: !buildProd,
+                    debug: !buildProd
                 }
             ],
             "stage-0",
