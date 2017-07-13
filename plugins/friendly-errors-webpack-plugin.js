aea788e90e5e0469e62882c46d7985b3e82f3b1f
LiGuoming <it1011@qq.com>
2017-07-13 18:17:07 +0800

    调整friendly消息

--- plugins/friendly-errors-webpack-plugin.js
+++ plugins/friendly-errors-webpack-plugin.js
@@ -1,19 +1,11 @@
-const {
-    port,
-    host,
-    analysis,
-    publicPath,
-    appRoot,
-    buildProd,
-    devServer
-} = global["talent-ui-runtime"];
+const { port, host, analysis, publicPath, appRoot, buildProd } = global[
+    "talent-ui-runtime"
+];
 const FriendlyErrorsWebpackPlugin = require("friendly-errors-webpack-plugin");
-var messages = [`任务：${devServer ? "开发服务器" : "项目构建"}`];
-if(devServer) messages.push(`本地服务地址：http://${host}:${port}`)
 
 module.exports = new FriendlyErrorsWebpackPlugin({
     compilationSuccessInfo: {
-        messages,
+        messages: [`成功启动本地开发服务器`, `地址：http://${host}:${port}`],
         notes: [
             `运行环境：${buildProd ? "production" : "development"}`,
             `根目录：'${appRoot}'`,
