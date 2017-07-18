a48f9b18fcf6c7e85a6dc4cbc29190f3f6fe8235
LiGuoming <it1011@qq.com>
2017-07-18 17:51:27 +0800

    修复图形资源输出目录不正确的问题

--- rules/file-rule.js
+++ rules/file-rule.js
@@ -2,6 +2,6 @@ module.exports = {
     test: /\.(png|jpg|gif|ttf|eot|svg|woff(2)?)(\?[^('|")]*)?$/,
     loader: "file-loader?name=images/[name].[ext]",
     options: {
-        name: "image/[name].[ext]"
+        name: "images/[name].[ext]"
     }
 };
