61661b61c5db67d0b5a0301621902c62e1e5bd22
LiGuoming <it1011@qq.com>
2017-07-20 18:30:15 +0800

    删除重复的代码

--- rules/file-rule.js
+++ rules/file-rule.js
@@ -1,6 +1,6 @@
 module.exports = {
     test: /\.(png|jpg|gif|ttf|eot|svg|woff(2)?)(\?[^('|")]*)?$/,
-    loader: "file-loader?name=images/[name].[ext]",
+    loader: "file-loader",
     options: {
         name: "images/[name].[ext]"
     }
