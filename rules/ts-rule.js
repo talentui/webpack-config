346aadefcb1ac5eb6729a849be1be6f4d2074051
LiGuoming <it1011@qq.com>
2017-08-11 18:52:05 +0800

    更新tsloader的配置

--- rules/ts-rule.js
+++ rules/ts-rule.js
@@ -1,12 +1,16 @@
 const { globalObjectKey, babelExclude } = require("../constants.js");
-const {buildProd} = global[globalObjectKey]
+const { buildProd } = global[globalObjectKey];
+const babelConfig = require("../helpers/babel-config.js");
 module.exports = {
     test: /\.tsx?$/,
-    loader: 'awesome-typescript-loader',
-    exlude: babelExclude,
+    loader: "awesome-typescript-loader",
+    exclude: babelExclude,
     options: {
         useBabel: true,
-        babelOptions:require('../helpers/babel-config.js'),
+        babelOptions: {
+            presets: babelConfig.presets,
+            plugins: babelConfig.plugins
+        },
         useCache: !buildProd
     }
-}
+};
