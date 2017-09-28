1e11eb1853d40bbf4f95eee54f8c2db92d860554
LiGuoming <it1011@qq.com>
2017-09-28 12:56:34 +0800

    解决在开发环境下使用webpack-dev-server会导致使用const让ie报错的问题

--- constants.js
+++ constants.js
@@ -10,7 +10,6 @@ module.exports = {
     extractStylePublicPath: '../',
     defaultEngines: ["react"],
     globalObjectKey: 'talent-ui-runtime',
-    babelExclude: /node_modules\/(?!@beisen\/talent-ui)/, //本正则由张跃同学提供
     languageJs: 'js',
     languageTs: 'ts',
     languageMixed: 'mixed',
--- rules/js-rule.js
+++ rules/js-rule.js
@@ -1,8 +1,12 @@
-const {babelExclude} = require('../constants')
-
 module.exports = {
     test: /\.(jsx?)$/,
-    exclude: babelExclude,
+    exclude: function(path) {
+        if (path.indexOf("node_modules/webpack-dev-server/client") !== -1 || path.indexOf("/node_modules/@beisen/talent-ui") !== -1) {
+            return false;
+        }
+
+        return path.indexOf("/node_modules/") !== -1;
+    },
     loader: "babel-loader",
-    options: require('../helpers/babel-config.js')
+    options: require("../helpers/babel-config.js")
 };
