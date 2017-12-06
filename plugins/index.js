2c1f24ad1ad9a3d10af29579c63dca0663209cdc
Raymond Li <it1011@qq.com>
2017-12-06 11:26:36 +0800

    fix(plugin): 只在生产环境下moduleConcatationPlugins，开发环境使用会导致块丢失

--- plugins/index.js
+++ plugins/index.js
@@ -6,35 +6,29 @@ var { devServer, useCommonChunk, analysis, friendly } = global[
 
 const enableCheck = process.env.check === "on";
 
-var plugins = [];
+var plugins = [require("./dll-reference-plugin"), require("./define-plugin")];
 
 //启用路径检查，因为在mac的文件系统下，不区分大小写
-if (enableCheck) plugins.push(require("./case-sensitive-path-plugin"));
 
 
-plugins.push(require('./module-concatenation-plugin'), require("./define-plugin"));
-
-plugins = plugins.concat(require("./dll-reference-plugin"));
+if (buildProd) {
+    plugins.push(require('./module-concatenation-plugin'), require("./extract-text-plugin"), require("./uglify-js-plugin"));
+}
 
 if (devServer) {
     plugins.push(
         require("./hot-module-replacement-plugin"),
-        require("./html-webpack-plugin")
+        require("./html-webpack-plugin"),
+        require("./add-asset-html-plugin")
     );
-    plugins = plugins.concat(require("./add-asset-html-plugin"));
 }
+
+if (useCommonChunk) plugins.push(require("./common-chunk-plugin"));
 
 if (friendly && devServer)
     plugins.push(require("./friendly-errors-webpack-plugin"));
 
-if (useCommonChunk) plugins.push(require("./common-chunk-plugin"));
-
-if (buildProd) {
-    plugins.push(require("./extract-text-plugin"));
-    plugins.push(
-        require("./uglify-js-plugin")
-    );
-}
+if (enableCheck) plugins.push(require("./case-sensitive-path-plugin"));
 
 if (analysis) {
     plugins.push(require("./bundle-analyzer-plugin"));
