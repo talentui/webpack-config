edd5082ff91ea4727815d97d68653a76fb5b6eff
Raymond Li <it1011@qq.com>
2017-12-06 14:32:05 +0800

    fix(plugin): 修复上次修改，plugins组合不正确的问题

--- plugins/index.js
+++ plugins/index.js
@@ -4,31 +4,40 @@ var { devServer, useCommonChunk, analysis, friendly } = global[
     globalObjectKey
 ];
 
+//使用push推进去的都是返回的单个plugins, 使用concat连接的都是返回的plugins集合
+
 const enableCheck = process.env.check === "on";
 
-var plugins = [require("./dll-reference-plugin"), require("./define-plugin")];
+var plugins = [];
 
 //启用路径检查，因为在mac的文件系统下，不区分大小写
+if (enableCheck) plugins.push(require("./case-sensitive-path-plugin"));
 
+if (buildProd) plugins.push(require('./module-concatenation-plugin'));
 
-if (buildProd) {
-    plugins.push(require('./module-concatenation-plugin'), require("./extract-text-plugin"), require("./uglify-js-plugin"));
-}
+plugins.push(require("./define-plugin"));
+
+plugins = plugins.concat(require("./dll-reference-plugin"));
 
 if (devServer) {
     plugins.push(
         require("./hot-module-replacement-plugin"),
-        require("./html-webpack-plugin"),
-        require("./add-asset-html-plugin")
+        require("./html-webpack-plugin")
     );
+    plugins = plugins.concat(require("./add-asset-html-plugin"));
 }
-
-if (useCommonChunk) plugins.push(require("./common-chunk-plugin"));
 
 if (friendly && devServer)
     plugins.push(require("./friendly-errors-webpack-plugin"));
 
-if (enableCheck) plugins.push(require("./case-sensitive-path-plugin"));
+if (useCommonChunk) plugins.push(require("./common-chunk-plugin"));
+
+if (buildProd) {
+    plugins.push(require("./extract-text-plugin"));
+    plugins.push(
+        require("./uglify-js-plugin")
+    );
+}
 
 if (analysis) {
     plugins.push(require("./bundle-analyzer-plugin"));
