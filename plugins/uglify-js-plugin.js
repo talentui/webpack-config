aa05ee88e7ad2df3cbf66283beda60c6c39cc3f6
Raymond Li <it1011@qq.com>
2018-07-02 17:06:00 +0800

    fix(plugin): 修复ie下报错的问题

--- plugins/uglify-js-plugin.js
+++ plugins/uglify-js-plugin.js
@@ -8,10 +8,11 @@
 //     output: { screw_ie8: false },
 //     sourceMap: true
 // });
+//如果使用6的话一些对象比如{a:a}会被转换成{a}
 module.exports = new (require('uglifyjs-webpack-plugin'))({
     uglifyOptions: {
         ie8: true,
-        ecma: 6,
+        ecma: 5, 
         compress: {
             drop_console: true
         }
