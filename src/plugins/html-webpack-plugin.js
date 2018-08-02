97b3b37ebf998ff43dbd980910367faebdc8addf
Raymond Li <it1011@qq.com>
2018-08-02 15:53:32 +0800

    fix(loader): 根据chunks调整html当中的显示顺序

--- src/plugins/html-webpack-plugin.js
+++ src/plugins/html-webpack-plugin.js
@@ -8,7 +8,7 @@ module.exports = new HtmlWebpackPlugin({
     filename: "index.html",
     inject: "body",
     chunksSortMode: function(a,b){
-        var order = ['webpack-bootstrap','common','main'];
+        var order = ['webpack-bootstrap','vendors','common','main'];
         return order.indexOf(a.names[0]) - order.indexOf(b.names[0]);
     }
 });
